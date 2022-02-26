6951469e8a76d5b0100254d18c638657839e5e6b
franciscoosnaya01 <francisco.osnaya@kidzania.com>
2022-02-26 00:06:00 -0600

    Add src of first.cpp

---
+++ first.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    system("clear");
+    cout << "FJOM te da la bienvenida a éste seguimiento de los ejercicios.\n";
+    cout << "Vamos a escribir más códigos en C++\n";
+
+    cout << "Presione ENTER para salir"; cin.get(); return 0;
+    return EXIT_SUCCESS;
+} 
