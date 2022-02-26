71169a2b826be454724448fbf7b862acfb7f5137
franciscoosnaya01 <francisco.osnaya@kidzania.com>
2022-02-26 00:17:53 -0600

    Primera parte del segundo ejercicio.

---
+++ second.cpp
@@ -0,0 +1,14 @@
+// Funcion -- Mostrar nombre y dirección
+#include <iostream>
+using namespace std;
+
+int main (int argc, char *argv[]){
+    system("clear");
+    cout << "Francisco Osnaya\n";
+    cout << "Calle de la amargura total.\n";
+    cout << "En algún lugar de la ciudad\n";
+    cout << "Centro histórico de tu corazón, México\n";
+    //system("pause");
+    cout << "Presione ENTER para salir"; cin.get(); return 0;
+    return EXIT_SUCCESS;
+}
